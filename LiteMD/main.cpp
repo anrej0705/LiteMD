25fe592e8af4a5c0b874e2fbeee47ac6b87d475c
Ilya <zaz128@yandex.ru>
2023-10-21 19:19:55 +0300

    -

--- LiteMD/main.cpp
+++ LiteMD/main.cpp
@@ -1,10 +1,10 @@
-#include "LiteMD.h"
+//#include "LiteMD.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    LiteMD w;
-    w.show();
+    //LiteMD w;
+    //w.show();
     return a.exec();
 }
