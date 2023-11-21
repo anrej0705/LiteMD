58beb2854adb81cb95f86611b4717b3c324b1740
Ilya <zaz128@yandex.ru>
2023-11-21 18:11:36 +0300

    Добавлена локаль ru_UA

---
+++ LiteMD/LiteMD_ru_UA.ts
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_UA">
+<context>
+    <name>LiteMD</name>
+    <message>
+        <source>Editor</source>
+        <translation>Редактор</translation>
+    </message>
+    <message>
+        <source>Viewer</source>
+        <translation>Рендер</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;О программе</translation>
+    </message>
+    <message>
+        <source>&amp;Open...</source>
+        <translation>&amp;Открыть...</translation>
+    </message>
+    <message>
+        <source>&amp;Save</source>
+        <translation>&amp;Сохранить</translation>
+    </message>
+    <message>
+        <source>S&amp;ave As...</source>
+        <translation>С&amp;охранить как...</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Выход</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;Настройки</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Помощь</translation>
+    </message>
+    <message>
+        <source>LiteMD alpha 0.1.3 build </source>
+        <translation>LiteMD alpha 0.1.3 сборка </translation>
+    </message>
+    <message>
+        <source>[MAX FILE SIZE 65K]</source>
+        <translation>[МАКС РАЗМЕР ФАЙЛА 65K]</translation>
+    </message>
+    <message>
+        <source>Ready</source>
+        <translation>Готов</translation>
+    </message>
+    <message>
+        <source>Ver. alpha 0.1.3 build </source>
+        <translation>Ver. alpha 0.1.3 сборка </translation>
+    </message>
+    <message>
+        <source>
+By anrej0705
+See me at Github:
+github.com/anrej0705
+
+This app is free for use,modify
+and reupload
+
+I hope u like my app :D</source>
+        <translation>
+Автор: anrej0705
+Ищи меня на Github:
+github.com/anrej0705
+
+Это приложение свободно для
+использования, модифицикации и
+распространения
+
+Надеюсь вам понравится моя
+прога :D</translation>
+    </message>
+</context>
+<context>
+    <name>LiteMDClass</name>
+    <message>
+        <source>LiteMD</source>
+        <translation>LiteMD</translation>
+    </message>
+</context>
+<context>
+    <name>mdEditor</name>
+    <message>
+        <source>Open Text/Markdown</source>
+        <translation>Открыть Text/Markdown</translation>
+    </message>
+    <message>
+        <source>*.md ;; *.txt</source>
+        <translation>*.md ;; *.txt</translation>
+    </message>
+    <message>
+        <source>Oversize detected</source>
+        <translation>Превышение размера</translation>
+    </message>
+    <message>
+        <source>Cannot open file because size of this is over </source>
+        <translation>Данный файл нельзя открыть так как его размер больше </translation>
+    </message>
+    <message>
+        <source> bytes</source>
+        <translation> байт</translation>
+    </message>
+    <message>
+        <source>Opened </source>
+        <translation>Открыто </translation>
+    </message>
+    <message>
+        <source>Save Text/Markdown</source>
+        <translation>Сохранить Text/Markdown</translation>
+    </message>
+    <message>
+        <source>Saved </source>
+        <translation>Сохранено </translation>
+    </message>
+</context>
+</TS>
--- LiteMD/main.cpp
+++ LiteMD/main.cpp
@@ -6,7 +6,26 @@
 int main(int argc, char *argv[])
 {
     QApplication mdApp(argc, argv);
-    LiteMD sdiEditor;
+	QTranslator lmd_lgn;
+	QString localeName = QLocale::system().name();
+	if (localeName == "en_US")
+	{
+		if (!lmd_lgn.load("LiteMD_en.qm", "."))
+			QErrorMessage::qtHandler();
+	}
+	else if (localeName == "ru_RU")
+	{
+		if (!lmd_lgn.load("LiteMD_ru.qm", "."))
+			QErrorMessage::qtHandler();
+	}
+	else if (localeName == "ru_UA")
+	{
+		if (!lmd_lgn.load("LiteMD_ru_UA.qm", "."))
+			QErrorMessage::qtHandler();
+	}
+	if (!mdApp.installTranslator(&lmd_lgn))
+		QErrorMessage::qtHandler();
+	LiteMD sdiEditor;
 	sdiEditor.resize(960, 576);
 	sdiEditor.show();
     return mdApp.exec();
