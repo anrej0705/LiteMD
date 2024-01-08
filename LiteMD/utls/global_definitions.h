8398c9eec48e57a06b28938e7901fac501a5bcc4
Ilya <zaz128@yandex.ru>
2024-01-08 11:37:02 +0300

    Шаблоны иконок

--- LiteMD/utls/global_definitions.h
+++ LiteMD/utls/global_definitions.h
@@ -1,7 +1,7 @@
 #pragma once
 #ifndef GLOBAL_DEFINITIONS_H_
 #define GLOBAL_DEFINITIONS_H_
-//Глобальные определения, в т.ч ИД событий
+//Глобальные определения, в т.ч версии сборки и ИД событий
 
 //---------------------------------------------
 //!!!НЕ ИЗМЕНЯЙ И НЕ ПЕРЕНОСИ ДАННУЮ СТРОЧКУ!!!
@@ -28,6 +28,7 @@
 
 #define SETTINGS_HEIGH									20
 
+//События смены языка
 #define APP_EVENT_UI_UPDATE_EVENT						33
 #define APP_EVENT_LiteMD_UPDATE_EVENT					34
 #define APP_EVENT_mdEditor_UPDATE_EVENT					35
@@ -36,5 +37,6 @@
 #define APP_EVENT_dialogBoxes_UPDATE_EVENT				38
 #define APP_EVENT_appSettings_UPDATE_EVENT				39
 #define APP_EVENT_tab_basic_UPDATE_EVENT				40
+//--------------------------------------------------------
 
 #endif
