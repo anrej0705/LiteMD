f7ad16f940f9e71f80e06fddd45fd28c8383c614
Ilya <zaz128@yandex.ru>
2024-11-23 04:03:15 +0300

    Исправлена многократная запись конфига

--- LiteMD/utls/xmlWriter.cpp
+++ LiteMD/utls/xmlWriter.cpp
@@ -9,55 +9,17 @@ extern struct parser_switchers parswitch;
 xmlWriter::xmlWriter()
 {
 	fileName = "config.xml";
-	lmdFileSet = new QDomDocument("LMD");
-	lmdSet = new QDomElement(lmdFileSet->createElement(appSign));
-	lmdFileSet->appendChild(*lmdSet);
-	QDomElement build = attrib(*lmdFileSet, "build", BUILD_NUMBER);
-	QDomElement patchNoteRead = attrib(*lmdFileSet, "patchNoteRead", logReadState);
-	QDomElement indevFeatures = attrib(*lmdFileSet, "enableIndevFeatures", enableIndevFeatures);
-	QDomElement deprFeatures = attrib(*lmdFileSet, "enableDeprFeatures", enableDeprFeatures);
-	QDomElement langCodeParam = attrib(*lmdFileSet, "langCode", langCode);
-	QDomElement enBasicUrlParse = attrib(*lmdFileSet, "enBasicUrlParse", parswitch.en_simple_url);
-	QDomElement enAdvUrlParse = attrib(*lmdFileSet, "enAdvUrlParse", parswitch.en_adv_url);
-	QDomElement enHeaderLvlParse = attrib(*lmdFileSet, "enHeaderLvlParse", parswitch.en_header_lvl);
-	lmdSet->appendChild(build);
-	lmdSet->appendChild(patchNoteRead);
-	lmdSet->appendChild(indevFeatures);
-	lmdSet->appendChild(deprFeatures);
-	lmdSet->appendChild(langCodeParam);
-	lmdSet->appendChild(enBasicUrlParse);
-	lmdSet->appendChild(enAdvUrlParse);
-	lmdSet->appendChild(enHeaderLvlParse);
 }
 
 xmlWriter::xmlWriter(QString fName)
 {
 	fileName = fName;
-	lmdFileSet = new QDomDocument("LMD");
-	lmdSet = new QDomElement(lmdFileSet->createElement(appSign));
-	lmdFileSet->appendChild(*lmdSet);
-	QDomElement build = attrib(*lmdFileSet, "build", BUILD_NUMBER);
-	QDomElement patchNoteRead = attrib(*lmdFileSet, "patchNoteRead", logReadState);
-	QDomElement indevFeatures = attrib(*lmdFileSet, "enableIndevFeatures", enableIndevFeatures);
-	QDomElement deprFeatures = attrib(*lmdFileSet, "enableDeprFeatures", enableDeprFeatures);
-	QDomElement langCodeParam = attrib(*lmdFileSet, "langCode", langCode);
-	QDomElement enBasicUrlParse = attrib(*lmdFileSet, "enBasicUrlParse", parswitch.en_simple_url);
-	QDomElement enAdvUrlParse = attrib(*lmdFileSet, "enAdvUrlParse", parswitch.en_adv_url);
-	QDomElement enHeaderLvlParse = attrib(*lmdFileSet, "enHeaderLvlParse", parswitch.en_header_lvl);
-	lmdSet->appendChild(build);
-	lmdSet->appendChild(patchNoteRead);
-	lmdSet->appendChild(indevFeatures);
-	lmdSet->appendChild(deprFeatures);
-	lmdSet->appendChild(langCodeParam);
-	lmdSet->appendChild(enBasicUrlParse);
-	lmdSet->appendChild(enAdvUrlParse);
-	lmdSet->appendChild(enHeaderLvlParse);
 }
 
 void xmlWriter::writeConfig()
 {
-	delete lmdFileSet;
-	delete lmdSet;
+	//delete lmdFileSet;
+	//delete lmdSet;
 	lmdFileSet = new QDomDocument("LMD");
 	lmdSet = new QDomElement(lmdFileSet->createElement(appSign));
 	lmdFileSet->appendChild(*lmdSet);
