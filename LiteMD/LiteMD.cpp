cf812dcc47f43071d6db9842d85005f6f4b57efa
Ilya <zaz128@yandex.ru>
2024-11-24 17:20:48 +0300

    Изменена компоновка заголовка приложения

--- LiteMD/LiteMD.cpp
+++ LiteMD/LiteMD.cpp
@@ -247,7 +247,7 @@ LiteMD::LiteMD(QWidget *parent) : QMainWindow(parent)
 	setCentralWidget(mainWgt);
 
 	//Устанавливаем заголовок окна
-	setWindowTitle(tr("LiteMD alpha 0.0.0 build ") + QString::number(static_cast<uint32_t>(BUILD_NUMBER))/* + tr("[MAX FILE SIZE 65K]")*/);
+	setWindowTitle(tr("LiteMD") + APP_STAGE + APP_VERSION + tr(" build ") + QString::number(static_cast<uint32_t>(BUILD_NUMBER))/* + tr("[MAX FILE SIZE 65K]")*/);
 
 	//Кешируем имя окна для возможности восстановления исходного заголовка
 	//defTitle = windowTitle();	//patch 0.2.2 исправление версии "0.0.0" при открытии файла
@@ -275,7 +275,7 @@ LiteMD::LiteMD(QWidget *parent) : QMainWindow(parent)
 //О программе
 void LiteMD::slotAbout()
 {
-	QMessageBox::about(this, "LiteMD", tr("Ver. alpha 0.0.0 build ") + QString::number(static_cast<uint32_t>(BUILD_NUMBER))
+	QMessageBox::about(this, "LiteMD", tr("Ver.") + APP_STAGE + APP_VERSION + (" build ") + QString::number(static_cast<uint32_t>(BUILD_NUMBER))
 		+ tr("<BR>By anrej0705<BR>See me at Github:") + "<BR><A HREF=\"github.com/anrej0705\">github.com/anrej0705</A><BR><BR>" 
 		+ tr("This app is free for use,modify and reupload<BR><BR>LiteMD IS FREE SOFTWARE! IF YOU PAID FOR IT YOU HAVE BEEN SCAMMED!") 
 		+ "<BR>" + "<BR>| Qt 5.14.2 | C++17 | C11 | Boost 1.84.00 |<BR>" + "<BR>" 
@@ -289,7 +289,7 @@ void LiteMD::slotTitleChanged(const QString& title)
 	std::string newTitle/* = defTitle.toStdString()*/;	//Патч 0.2.2 исправление заголовка
 	std::string fileFullPath = title.toStdString();
 	//Формируем заголовок из контейнеров и устанавливаем в приложение
-	newTitle.append(tr("LiteMD alpha 0.0.0 build ").toStdString() + " [" + fileFullPath.substr(fileFullPath.rfind('/') + 1, fileFullPath.size() - fileFullPath.rfind('/')) + "]");
+	newTitle.append((tr("LiteMD") + APP_STAGE + APP_VERSION + tr(" build ") + QString::number(static_cast<uint32_t>(BUILD_NUMBER))).toStdString() + " [" + fileFullPath.substr(fileFullPath.rfind('/') + 1, fileFullPath.size() - fileFullPath.rfind('/')) + "]");
 	setWindowTitle(QString::fromStdString(newTitle));
 }
 //Сброс заголовка
