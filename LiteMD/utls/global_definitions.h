290400d9a5125397108c4c87188614b76ab4bafa
Ilya <zaz128@yandex.ru>
2024-12-16 12:41:31 +0300

    Обновлено количество параметров конфига

--- LiteMD/utls/global_definitions.h
+++ LiteMD/utls/global_definitions.h
@@ -22,7 +22,7 @@
 const char appSign[] = "__Shani_basic";
 
 //Количество читаемых параметров из конфига. Прибавлять каждый раз когда добавляется новая фича
-#define PARAM_CNT	16
+#define PARAM_CNT	17
 
 /*
 * Таблица событий для обновления интерфейса
@@ -85,6 +85,9 @@ const char striked_txt_iclose[] = "</s>";
 const char underlined_txt_iopen[] = "<ins>";
 const char underlined_txt_iclose[] = "</ins>";
 
+const char italic_txt_iopen[] = "<I>";
+const char italic_txt_iclose[] = "</I>";
+
 const uint8_t simple_url_iopenurl_size = 9;
 const uint8_t simple_url_icloseurl_size = 2;
 const uint8_t simple_url_iclosetext_size = 4;
@@ -98,4 +101,7 @@ const uint8_t striked_txt_iclose_size = 4;
 
 const uint8_t underlined_txt_iopen_size = 5;
 const uint8_t underlined_txt_iclose_size = 6;
+
+const uint8_t italic_txt_iopen_size = 3;
+const uint8_t italic_txt_iclose_size = 4;
 #endif
